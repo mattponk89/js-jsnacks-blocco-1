8bf59af43398027b55678d8f6dd4d70f6fc3757b
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-10 16:43:42 +0000

    fix esercizio 2
    Mi dimenticavo di far partire lo script quando la pagina è caricata del tutto.

--- jsnack-{2}/script.js
+++ jsnack-{2}/script.js
@@ -1,44 +1,46 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici
 // da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+$(document).ready(function () {
+  var bici = [{
+    'nome' : 'Graziella',
+    'peso' : 10
+    },
+    {
+    'nome' : 'Maria',
+    'peso' : 14
+    },
+    {
+    'nome' : 'Graziella',
+    'peso' : 10
+    },
+    {
+    'nome' : 'Pippo',
+    'peso' : 9
+    }
 
-var bici = [{
-  'nome' : 'Graziella',
-  'peso' : 10
-  },
-  {
-  'nome' : 'Maria',
-  'peso' : 14
-  },
-  {
-  'nome' : 'Graziella',
-  'peso' : 10
-  },
-  {
-  'nome' : 'Pippo',
-  'peso' : 9
+  ]
+  var bestBici = {
+    'biciLeggera' : [],
+    'pesoMin' : undefined
+  };
+  for(var i=0; i < bici.length; i++){
+    if(bestBici.pesoMin == undefined){
+      bestBici.pesoMin = bici[i].peso
+    } else if (bici[i].peso == bestBici.pesoMin){
+      bestBici.biciLeggera.push(i)
+    } else if (bici[i].peso < bestBici.pesoMin){
+      bestBici.pesoMin = bici[i].peso
+      bestBici.biciLeggera = [i]
+    }
   }
-
-]
-var bestBici = {
-  'biciLeggera' : [],
-  'pesoMin' : undefined
-};
-for(var i=0; i < bici.length; i++){
-  if(bestBici.pesoMin == undefined){
-    bestBici.pesoMin = bici[i].peso
-  } else if (bici[i].peso == bestBici.pesoMin){
-    bestBici.biciLeggera.push(i)
-  } else if (bici[i].peso < bestBici.pesoMin){
-    bestBici.pesoMin = bici[i].peso
-    bestBici.biciLeggera = [i]
+  var boxEl = $('#box');
+  if(bestBici.biciLeggera.length == 1){
+    boxEl.append('<h2>Il nome della bici leggera è: ' + bici[bestBici.biciLeggera[0]].nome + ' il suo peso è di: ' + bestBici.pesoMin + '</h2>');
+  } else if ( bestBici.biciLeggera.length > 1){
+    for(var j = 0; j < bestBici.biciLeggera.length; j++){
+      boxEl.append('<h2>Parimerito di: '+ bici[bestBici.biciLeggera[j]].nome + 'il peso di: ' + bestBici.pesoMin +'</h2>');
+      console.log('più bici leggere');
+    }
   }
-}
-
-if(bestBici.biciLeggera.length == 1){
-  $('#stampa').html('Il nome della bici leggera è: ' + bici[bestBici.biciLeggera[0]].nome + ' il suo peso è di: ' + bestBici.pesoMin);
-} else if ( bestBici.biciLeggera.length > 1){
-  for(var j = 0; j < bestBici.biciLeggera.length; j++){
-    $('#stampa').append('<h2>Parimerito di: '+ bici[bestBici.biciLeggera[j]].nome + 'il peso di: ' + bestBici.pesoMin +'</h2>');
-  }
-}
+})
