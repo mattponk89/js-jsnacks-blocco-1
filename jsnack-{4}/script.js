38e50e763bb8e1b4e9cb8db61bc2ed05b3e36195
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-11-10 16:27:48 +0000

    Finito 4 Manca solo fix il 2

--- jsnack-{4}/script.js
+++ jsnack-{4}/script.js
@@ -4,7 +4,7 @@
 // Nome sarà l’unica proprietà da compilare, le altre
 // saranno tutte settate a 0.
 
-squadre [
+var squadre = [
   { 'nome': 'Juve', 'punti': 0, 'falliSubiti': 0},
   { 'nome': 'Inter', 'punti': 0, 'falliSubiti': 0},
   { 'nome': 'Milan', 'punti': 0, 'falliSubiti': 0}
@@ -12,3 +12,12 @@ squadre [
 
 // Generare numeri random al posto degli 0 nelle proprietà:
 // punti fatti e falli subiti
+
+function genNum(){
+  return Math.floor((Math.random() * 100) + 1);
+}
+for(var i=0; i < squadre.length; i++){
+  squadre[i].punti = genNum();
+  squadre[i].falliSubiti = genNum();
+  console.log('Nome: ' + squadre[i].nome + ' Punti: ' + squadre[i].punti + ' Falli Subiti: '+ squadre[i].falliSubiti)
+}
